a2111e1ce3f3627d55194c8c6e1f3afae18b1dc1
Polina Kolesnikova <polina@example.com>
2024-01-03 22:22:56 +0100

    adding userName

--- app/src/main/java/com/example/cafedd/MainActivity.java
+++ app/src/main/java/com/example/cafedd/MainActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +15,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, OrderDetailedActivity.class);
+
+
+        Button btnEnter = findViewById(R.id.enterBtn);
+        EditText password = findViewById(R.id.password);
+        EditText name = findViewById(R.id.userName);
+
+        btnEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userPassword = password.getText().toString().trim();
+                String userName = name.getText().toString().trim();
+                if(userName.isEmpty() || userPassword.isEmpty()){
+                    Toast.makeText(
+                            MainActivity.this,
+                            R.string.error_empty_fields,
+                            Toast.LENGTH_SHORT).show();
+                }
+                else lanchNewActivity(userName);
+
+
+            }
+        });
+
+    }
+
+    private void lanchNewActivity(String userName) {
+        Intent intent = MakeOrderActivity.newIntent(this, userName);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/cafedd/MakeOrderActivity.java
+++ app/src/main/java/com/example/cafedd/MakeOrderActivity.java
@@ -2,13 +2,63 @@ package com.example.cafedd;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
+
 
 public class MakeOrderActivity extends AppCompatActivity {
+    private static final String EXTRA_USER_NAME = "userName";
+    private TextView tvGreeting;
+    private RadioGroup drink;
+    private RadioButton teaBtn;
+    private RadioButton coffeeBtn;
+    private CheckBox sugar;
+    private CheckBox milk;
+    private CheckBox lemon;
+    private Spinner tea;
+    private Spinner coffee;
+    private Button makeOrder;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_order);
+        Intent intent = getIntent();
+        initViews();
+        String userName = intent.getStringExtra(EXTRA_USER_NAME);
+        String greeting = getString(R.string.hello_s_what_do_you_want, userName);
+        tvGreeting.setText(greeting);
+
+
+
+    }
+
+    private void initViews() {
+        tvGreeting = findViewById(R.id.tvGreeting);
+        drink = findViewById(R.id.rbDrink);
+        teaBtn = findViewById(R.id.rbTea);
+        coffeeBtn = findViewById(R.id.rbCoffee);
+        sugar = findViewById(R.id.sugar);
+        milk = findViewById(R.id.milk);
+        lemon = findViewById(R.id.lemon);
+        tea = findViewById(R.id.spinnerTea);
+        coffee = findViewById(R.id.spinnerCoffie);
+        makeOrder = findViewById(R.id.btnMakeOrder);
+
+    }
+
+    public static Intent newIntent(Context context, String userName){
+        Intent intent = new Intent(context, MakeOrderActivity.class);
+        intent.putExtra(EXTRA_USER_NAME, userName);
+        return intent;
     }
 }
