b88c355008d4511c0354924050e740802d421fe0
Polina Kolesnikova <polina@example.com>
2024-01-04 13:56:23 +0100

    logic of 2 and 3 screen

--- app/src/main/java/com/example/cafedd/MakeOrderActivity.java
+++ app/src/main/java/com/example/cafedd/MakeOrderActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.RadioButton;
@@ -12,19 +13,26 @@ import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 
 public class MakeOrderActivity extends AppCompatActivity {
     private static final String EXTRA_USER_NAME = "userName";
     private TextView tvGreeting;
+    private String userName;
     private RadioGroup drink;
     private RadioButton teaBtn;
     private RadioButton coffeeBtn;
+    private TextView userChoseDrink;
     private CheckBox sugar;
     private CheckBox milk;
     private CheckBox lemon;
     private Spinner tea;
     private Spinner coffee;
     private Button makeOrder;
+    private String userDrink;
+    private String drinkType;
+    private ArrayList<String> additions;
 
 
 
@@ -32,14 +40,88 @@ public class MakeOrderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_order);
-        Intent intent = getIntent();
+
         initViews();
-        String userName = intent.getStringExtra(EXTRA_USER_NAME);
+        setUpName();
+
+
+        drink.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int id) {
+                if(teaBtn.getId()==id){
+                    onUserChoseTea();
+                }else onUserChoseCoffee();
+            }
+        });
+
+        teaBtn.setChecked(true);
+
+        makeOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onMakeUserOrder();
+                lanchNewActivity();
+            }
+        });
+
+
+    }
+
+    private void lanchNewActivity() {
+        Intent intent = OrderDetailedActivity.newIntent(this, userName, userDrink, additions, drinkType);
+        startActivity(intent);
+    }
+
+    private void onMakeUserOrder() {
+        additions = new ArrayList<>();
+        if(sugar.isChecked()){
+            additions.add(sugar.getText().toString());
+        }
+
+        if(milk.isChecked()){
+            additions.add(milk.getText().toString());
+        }
+
+        if(teaBtn.isChecked() && lemon.isChecked()){
+            additions.add(lemon.getText().toString());
+        }
+
+
+
+        if(teaBtn.isChecked()){
+            drinkType = tea.getSelectedItem().toString();
+        } else if (coffeeBtn.isChecked()) {
+            drinkType = coffee.getSelectedItem().toString();
+        }
+
+    }
+
+    private void onUserChoseCoffee() {
+        userDrink = getString(R.string.coffee);
+        String additivesLabel = getString(R.string.additives, userDrink);
+        userChoseDrink.setText(additivesLabel);
+        lemon.setVisibility(View.INVISIBLE);
+
+        tea.setVisibility(View.INVISIBLE);
+        coffee.setVisibility(View.VISIBLE);
+
+
+    }
+
+    private void onUserChoseTea() {
+        userDrink = getString(R.string.tea);
+        String additivesLabel = getString(R.string.additives, userDrink);
+        userChoseDrink.setText(additivesLabel);
+        lemon.setVisibility(View.VISIBLE);
+        tea.setVisibility(View.VISIBLE);
+        coffee.setVisibility(View.INVISIBLE);
+    }
+
+    private void setUpName() {
+        Intent intent = getIntent();
+        userName = intent.getStringExtra(EXTRA_USER_NAME);
         String greeting = getString(R.string.hello_s_what_do_you_want, userName);
         tvGreeting.setText(greeting);
-
-
-
     }
 
     private void initViews() {
@@ -47,6 +129,7 @@ public class MakeOrderActivity extends AppCompatActivity {
         drink = findViewById(R.id.rbDrink);
         teaBtn = findViewById(R.id.rbTea);
         coffeeBtn = findViewById(R.id.rbCoffee);
+        userChoseDrink = findViewById(R.id.drink);
         sugar = findViewById(R.id.sugar);
         milk = findViewById(R.id.milk);
         lemon = findViewById(R.id.lemon);
--- app/src/main/java/com/example/cafedd/OrderDetailedActivity.java
+++ app/src/main/java/com/example/cafedd/OrderDetailedActivity.java
@@ -2,13 +2,68 @@ package com.example.cafedd;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+
 
 public class OrderDetailedActivity extends AppCompatActivity {
+    public static final String EXTRA_USER_NAME = "userName";
+    private static final String EXTRA_USER_DRINK = "userDrink";
+    private static final String EXTRA_USER_ADDITIONS = "userAdditions";
+    private static final String EXTRA_USER_DRINK_TYPE = "userDrinkType";
+
+    private String userName;
+    private String userDrink;
+    private String additions;
+    private String drinkType;
+
+    private TextView userNameView;
+    private TextView userDrinkView;
+    private TextView userDrinkType;
+    private TextView userDrinkAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_detailed);
+
+        setUpVariables();
+        setUpFields();
+
+    }
+
+    private void setUpFields() {
+        userNameView = findViewById(R.id.textViewName_res);
+        userDrinkView = findViewById(R.id.textViewDrink_res);
+        userDrinkType = findViewById(R.id.textViewTypeDrink_res);
+        userDrinkAdd = findViewById(R.id.textViewAdditivies_res);
+
+        userNameView.setText(userName);
+        userDrinkView.setText(userDrink);
+        userDrinkType.setText(drinkType);
+        userDrinkAdd.setText(additions);
+
+    }
+
+    private void setUpVariables() {
+        userName = getIntent().getStringExtra(EXTRA_USER_NAME);
+        userDrink = getIntent().getStringExtra(EXTRA_USER_DRINK);
+        additions = getIntent().getStringExtra(EXTRA_USER_ADDITIONS);
+        drinkType = getIntent().getStringExtra(EXTRA_USER_DRINK_TYPE);
+    }
+
+
+    public static Intent newIntent(Context context, String userName, String userDrink, ArrayList<String> additions, String drinkType){
+        Intent intent = new Intent(context, OrderDetailedActivity.class);
+        intent.putExtra(EXTRA_USER_NAME, userName);
+        intent.putExtra(EXTRA_USER_DRINK, userDrink);
+        intent.putExtra(EXTRA_USER_ADDITIONS, additions.toString());
+        intent.putExtra(EXTRA_USER_DRINK_TYPE, drinkType);
+        return intent;
     }
 }
